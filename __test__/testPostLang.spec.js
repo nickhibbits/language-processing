a06b0d4cb16069c96edf428839d2842cf615b678
nicholas-hibbits <nicholashibbits@gmail.com>
2021-03-10 07:48:27 -0800

    Continued work on validating user input with regex

--- __test__/testPostLang.spec.js
+++ __test__/testPostLang.spec.js
@@ -10,7 +10,6 @@ describe("Testing the submit functionality", () => {
           const output = [{
           }];
           try {
-            is_url(input.url);
             expect(postData(input)).toBe(output);
             done();
           } catch (error) {
@@ -19,19 +18,6 @@ describe("Testing the submit functionality", () => {
         }
     );
 });
-
-function is_url(str)
-{
-  const regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
-        if (regexp.test(str))
-        {
-          return true;
-        }
-        else
-        {
-          return false;
-        }
-}
 
 
 
--- src/client/js/postLang.js
+++ src/client/js/postLang.js
@@ -1,21 +1,22 @@
 // POST request to retrieve language of the user's input
 async function postData(url = "", newInfo = {}) {
-    is_url(url);
-    const response = await fetch(url, {
-        method: "POST",
-        credentials: "same-origin",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        // Body data type must match "Content-Type" header
-        body: JSON.stringify(newInfo),
-    });
-    try {
-        const newData = await response.json();
-        console.log(newData);
-        return newData;
-    } catch (error) {
-        console.log("error", error);
+    if (is_url(newInfo['formText']));{
+      const response = await fetch(url, {
+          method: "POST",
+          credentials: "same-origin",
+          headers: {
+              "Content-Type": "application/json"
+            },
+            // Body data type must match "Content-Type" header
+          body: JSON.stringify(newInfo),
+          });
+      try {
+          const newData = await response.json();
+          console.log(newData);
+          return newData;
+      } catch (error) {
+          console.log("error", error);
+        }
     }
 };
 
