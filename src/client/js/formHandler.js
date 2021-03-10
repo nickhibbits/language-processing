7b485f6bf973b8fd77076de6200403c2963f26e4
nicholas-hibbits <nicholashibbits@gmail.com>
2021-03-10 09:24:58 -0800

    Changed location of input validation to formHandler

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -6,8 +6,10 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value;
-    postData('http://localhost:8081/language', {formText})
-    .then(() => update());
+    if (is_url(formText)) {
+      postData('http://localhost:8081/language', {formText})
+      .then(() => update());
+    }
     // if (update() != false && postData() != false) {
     //   // return 'success'
     //   console.log("Call and update successful")
@@ -15,5 +17,17 @@ function handleSubmit(event) {
     // else{console.log("error")}
 }
 
+function is_url(str)
+{
+  const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+        if (regexp.test(str))
+        {
+          return true;
+        }
+        else
+        {
+          return alert("Invalid input");
+        }
+}
 
 export { handleSubmit }
